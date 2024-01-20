f98229c97d5527d52ac69567e23d911568e6ef41
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2024-01-20 09:06:12 +0000

    json: Add list of jokes

---
+++ jokes.py
@@ -0,0 +1,13 @@
+import json
+
+jokes = [
+    "I told my wife she was drawing her eyebrows too high.She looked surprised.",
+    "Why did the bicycle fall over? \n Because it was two-tired!",
+    "What do you call fake spaghetti? \n An impasta.",
+    "I only know 25 letters of the alphabet.\n I don\'t know y.",
+    "Why don't scientists trust atoms? \n Because they make up everything.",
+    "What did one wall say to the other wall? \n I\'ll meet you at the corner.",
+    "Why couldn't the bicycle stand up by itself? \n It was two-tired.",
+    "What do you call a fish wearing a bowtie? \n SoFISHticated.",
+    "I used to play piano by ear, but now I use my hands and fingers."
+]
